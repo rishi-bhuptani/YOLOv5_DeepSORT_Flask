476d2aefa7f3a0dcdd015c69e8ac6845316630d3
Chelton Kuang <69950145+ckuang98@users.noreply.github.com>
2022-05-08 13:46:04 -0700

    Update detect_track.py

--- detect_track.py
+++ detect_track.py
@@ -73,7 +73,7 @@ def run(
         visualize=False,  # visualize features
         update=False,  # update all models
         project=ROOT / 'runs/detect',  # save results to project/name
-        name='exp',  # save results to project/name
+        name='',  # save results to project/name
         exist_ok=False,  # existing project/name ok, do not increment
         line_thickness=3,  # bounding box thickness (pixels)
         hide_labels=False,  # hide labels
